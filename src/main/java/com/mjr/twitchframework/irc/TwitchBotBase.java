dc3d1ea29d197579642adc7fc472fa619403c364
MJRLegends <MJRLegends@users.noreply.github.com>
2019-07-16 03:25:32 +0100

    Added channelName, channelID data to Moved channelName and channelID for twitch back to TwitchBotBase

--- src/main/java/com/mjr/twitchframework/irc/TwitchBotBase.java
+++ src/main/java/com/mjr/twitchframework/irc/TwitchBotBase.java
@@ -2,9 +2,34 @@ package com.mjr.twitchframework.irc;
 
 public abstract class TwitchBotBase {
 
+	private int channelID;
+	private String channelName;
+
+	public int getChannelID() {
+		return this.channelID;
+	}
+
+	public void setChannelID(int channelID) {
+		this.channelID = channelID;
+	}
+
+	public String getChannelName() {
+		return this.channelName;
+	}
+
+	public void setChannelName(String channelName) {
+		this.channelName = channelName;
+	}
+
+	public String getIrcChannelName() {
+		return "#" + this.getChannelName();
+	}
+
 	public abstract void onMessage(final String channel, final String sender, final String login, final String hostname, final String userID, final boolean subscriber, final String message);
 
-	/** Same as onMessage but includes the rawMessage for extra parsing
+	/**
+	 * Same as onMessage but includes the rawMessage for extra parsing
+	 * 
 	 * @param line
 	 * @param channel
 	 * @param sender
