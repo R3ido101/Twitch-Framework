86daf5fea34ec8c6599f8cceb8e212378adf30a3
MJRLegends <MJRLegends@users.noreply.github.com>
2020-03-11 19:55:06 +0000

    Added comment to IRCDisconnectEvent to explain use case

--- src/main/java/com/mjr/twitchframework/irc/events/IRCDisconnectEvent.java
+++ src/main/java/com/mjr/twitchframework/irc/events/IRCDisconnectEvent.java
@@ -3,6 +3,9 @@ package com.mjr.twitchframework.irc.events;
 import com.mjr.twitchframework.Event;
 import com.mjr.twitchframework.irc.TwitchIRCClient;
 
+/**
+ * Reconnecting of IRC Connections is done automatically by the Framework, this event is designed to be used for notification of disconnect
+ */
 public class IRCDisconnectEvent extends Event {
 
 	public final TwitchIRCClient client;
