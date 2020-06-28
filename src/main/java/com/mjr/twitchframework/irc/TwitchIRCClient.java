e8b9d02b46dc3b4f576137211303dae7f0203c8c
MJRLegends <MJRLegends@users.noreply.github.com>
2020-06-28 01:25:09 +0100

    Revert "IRC: Fixed request capabilities not being requested on adding channels to an already existing connection"
    This reverts commit 430cf836a08be129a1c526762d08f5715f33f26a.

--- src/main/java/com/mjr/twitchframework/irc/TwitchIRCClient.java
+++ src/main/java/com/mjr/twitchframework/irc/TwitchIRCClient.java
@@ -152,7 +152,6 @@ public class TwitchIRCClient extends PircBot {
 	public void addChannelWithConnect(String channel) {
 		this.channels.add(channel);
 		this.joinChannel("#" + channel);
-		requestCapabilities();
 	}
 
 	public void removeChannelWithDisconnect(String channel) {
